a6d506018d70f01f8412510988b95dc1bc54e90b
RacovitaCristina <cristinaracovita6125@yahoo.com>
2024-03-13 17:30:18 +0400

    lab3£

---
+++ lab3/Math/Math.cpp
@@ -0,0 +1,58 @@
+#include "Math.h"
+
+static int Math::Add(int a, int b)
+{
+	return a + b;
+
+}
+
+static int Math::Add(int a, int b,int c)
+{
+	return a + b+c;
+
+}
+
+static int Math::Add(double a, double b)
+{
+	return a + b;
+
+}
+
+static int Math::Add(double a, double b,double c)
+{
+	return a + b + c;
+
+}
+
+
+
+static int Math::Mul(int a, int b)
+{
+	return a * b;
+
+}
+
+static int Math::Mul(int a, int b, int c)
+{
+	return a * b * c;
+
+}
+
+static int Math::Mul(double a, double b)
+{
+	return a * b;
+
+}
+
+static int Math::Mul(double a, double b, double c)
+{
+	return a * b * c;
+
+}
+
+
+static int Math::Add(int count, ...)
+{
+
+
+}
---
+++ lab3/Math/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<Add(2, 5);
+	cout<<Add(2, 5, 6);
+	cout<<Add(2.5, 5.3);
+	cout<<Add(2.5, 5.3, 6.2);
+	cout << Mul(2, 3);
+	cout << Mul(2, 6, 9);
+	cout << Mul(2.5, 2.3);
+	cout << Mul(2.5, 5.6, 2.3);
+
+
+
+}
